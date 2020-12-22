d5b8ee0f935439f2a9f55a8e9df83b186d8269c2
Rabi Siddique <60459172+Rabi27@users.noreply.github.com>
2020-12-22 19:55:24 +0500

    BST Implementation with Insertion and Deletion

--- Python/Trees/BinarySearchTree.py
+++ Python/Trees/BinarySearchTree.py
@@ -79,23 +79,83 @@ class Node():
             self.preorder(root.left)
             self.preorder(root.right)
 
-'''
-Deletion of a node in a BST
-case 1 : Node has no children. Its a leaf node.
-case 2: Node has a single child.
-case 3 : Node has two children.
+    '''
+    Deletion of a node in a BST
+    case 1 : Node has no children. Its a leaf node.
+    case 2: Node has a single child.
+    case 3 : Node has two children.
 
-case 1 : Just remove the link from the parent to the child. Parent node
-will now point to null/None.
+    case 1 : Just remove the link from the parent to the child. Parent node
+    will now point to null/None.
 
-case 2: Replace the node with its only child.
+    case 2: Replace the node with its only child.
 
-case 3: Replace with the inorder successor so as to maintain the
-inorder traversal of the tree.
+    case 3: Replace with the inorder successor so as to maintain the
+    inorder traversal of the tree.
 
-'''
+    '''
 
+    #Method to find the minimum node value in the BST
+    def minValueNode(self,node):
+        current = node
     
+        # loop down to find the leftmost leaf
+        while(current.left is not None):
+            current = current.left
+    
+        return current
+
+    #Method to find the maximum node value in the BST
+    def max(self):
+        if self.right == None:
+            return self.data
+        return self.right.max()
+
+    def deleteNode(self,root, key):
+ 
+        # Base Case
+        if root is None:
+            return root
+    
+       
+        #If the key to be deleted less than root node than goto the left substree.
+        if key < root.data:
+            root.left = self.deleteNode(root.left, key)
+    
+        #If the key to be deleted greater than root node than goto the right substree.
+        elif(key > root.data):
+            root.right = self.deleteNode(root.right, key)
+    
+        # If key is same as root's key, then this is the node to be deleted
+        else:
+    
+            # Node with only one child or no child
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+    
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+    
+            # Node with two children: 
+            # Get the inorder successor
+            # (smallest in the right subtree)
+            temp = self.minValueNode(root.right)
+    
+            # Copy the inorder successor's 
+            # content to this node
+            root.data = temp.data
+    
+            # Delete the inorder successor
+            root.right = self.deleteNode(root.right, temp.data)
+    
+        return root
+        
+
+
 
 root = Node(27)
 root.insert(14)
@@ -106,13 +166,17 @@ root.insert(31)
 root.insert(42)
 print("Inorder Traversal:")
 root.inorder(root)
+
+'''
 print("Preorder Traversal:")
 root.preorder(root)
 print("Postorder Traversal:")
 root.postorder(root)
+'''
 
-
-            
+print("Inorder Traversal after Deletion")
+root.deleteNode(root,19)
+root.inorder(root)
 
         
 
