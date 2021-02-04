f37e9a9ce194e82f6517acebd2e5c9e62bac7321
Rabi Siddique <60459172+Rabi27@users.noreply.github.com>
2021-02-04 17:08:27 +0500

    Count the number of ways to make word using a string array with recursion and memoization

---
+++ Python/Recursion/countConstructs.py
@@ -0,0 +1,41 @@
+#count the number of construct a given word from a given array of strings
+def countConstruct(target,words):
+    
+    if target == "":
+        return 1
+    
+    count = 0
+    
+    for word in words:
+        if target.find(word) == 0:
+            suffix = target[len(word):]
+            if countConstruct(suffix,words) == 1:
+                count += 1
+    
+    return count
+
+print(countConstruct('abcdef',['ab','abc','cd','def','abcd']))
+print(countConstruct('skateboard',['bo','rd','ate','t','ska','sk','boar']))
+print(countConstruct('enterpotentpot',['a','p','ent','enter','ot','o','t']))
+print(countConstruct('rabi',['a','ra','i']))
+print(countConstruct('rabi',['a','ra','i','b']))
+print(countConstruct('purple',['purp','p','ur','le','purpl']))
+
+#Memoied Version
+memo = {}
+def countConstruct_2(target,words):
+    if target in memo: return memo[target]
+    if target == "":
+        return 1
+    
+    count = 0
+    
+    for word in words:
+        if target.find(word) == 0:
+            suffix = target[len(word):]
+            if countConstruct_2(suffix,words) == 1:
+                count += 1
+    memo[target] = count
+    return count
+
+print(countConstruct_2('purple',['purp','p','ur','le','purpl']))
